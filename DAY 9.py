e4ff8d9ca80374d4e44636f2f3d20376ca3e5ac2
Ashwini Mohan Giri <ashwinigiri203205@gmail.com>
2026-01-28 19:04:43 +0530

    Refactor and clean up while loop examples

--- DAY 9.py
+++ DAY 9.py
@@ -1,5 +1,5 @@
 """ LOOPS   
-for loop, while loop, nested loop
+for loop, while loop
 Lops are use to repeat a instruction or block of code multiple times 
 """
 
@@ -98,4 +98,5 @@ while count<2:                    #WHILE CONDITION, PRINT HELLO YASH
     if count==1:                   #WHEN COUNT IS 1, SKIP THE PRINT STATEMENT
         continue
     print("Hello Yash")
+
 
