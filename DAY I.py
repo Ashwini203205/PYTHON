391bb0f3935b55a837f15df9dde9134aa9d9bb67
Ashwini Mohan Giri <ashwinigiri203205@gmail.com>
2026-02-17 11:23:41 +0530

    Add files via upload

---
+++ DAY I.py
@@ -0,0 +1,45 @@
+# Polymorphism
+"""
+poly:many & morphism:form
+operator overloading
+when the same operator is allowed to have diff meaning acc to context
+
+There are mainly 2 types: Compile-time Polymorphism (Method Overloading)
+                          Run-time Polymorphism (Method Overriding)
+"""
+# Operation polym.    (+ overload , diff form diff meaning) 
+
+print(1+2)          #add:3
+print("ash"+"wini") #concatination:ashwini
+print([1,2]+[3,4])  #merger:[1,2,3,4]
+
+
+# Compile-time Polymorphism (Method Overloading)
+
+class Math:
+    def add(self, a, b, c=0):
+        return a + b + c
+
+m = Math()
+print(m.add(2, 3))      # 5
+print(m.add(2, 3, 4))   # 9
+
+
+# Run-Time Polymorphism(Method Overriding)
+
+class Parent:
+    def show(self):
+        print("This is Parent class")
+
+class Child(Parent):
+    def show(self):
+        print("This is Child class")
+
+c = Child()
+c.show()
+
+
+# Function Polymorphism
+print(len("Hello"))
+print(len([1, 2, 3, 4]))
+print(type("hello"))
